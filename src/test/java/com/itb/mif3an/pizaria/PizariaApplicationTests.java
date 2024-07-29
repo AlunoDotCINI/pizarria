0c3f52a3edc9ee1b730cda1aa24f3a60cee1a376
João <joaovictorgacunha@gmail.com>
2024-07-29 20:02:45 -0300

    Inicio ambiente de desenvolvimento

---
+++ src/test/java/com/itb/mif3an/pizaria/PizariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.pizaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
